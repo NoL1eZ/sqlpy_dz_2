ce4fba8b65133059d3c3e4f896bb4bb89083047d
NoL1eZ <108579116+NoL1eZ@users.noreply.github.com>
2022-09-12 00:06:45 +0300

    Update Script-1.sql

--- Script-1.sql
+++ Script-1.sql
@@ -1,33 +1,26 @@
-CREATE TABLE IF NOT EXISTS Genre_list (
-	genre_list_id SERIAL PRIMARY KEY,
-	genre VARCHAR(40) NOT NULL REFERENCES Genre(genre_id)
-);
-
-CREATE TABLE IF NOT EXISTS Genre (
+CREATE TABLE IF NOT EXISTS Genres
 	genre_id SERIAL PRIMARY KEY,
-	artist VARCHAR(40) NOT NULL REFERENCES Artist(artist_id)
+	artist VARCHAR(40) NOT NULL 
 );
 
-CREATE TABLE IF NOT EXISTS Artist_list (
-	artist_list_id SERIAL PRIMARY KEY,
-	artist VARCHAR(40) NOT NULL REFERENCES Artist(artist_id)
+CREATE TABLE IF NOT EXISTS Artist_genre (
+	artist_genre_id SERIAL PRIMARY KEY,
+	artist VARCHAR(40) NOT NULL REFERENCES Artists(artist_id),
+	genre VARCHAR(40) NOT NULL REFERENCES Genres(genre_id)
 );
 
-CREATE TABLE IF NOT EXISTS Artist (
+
+CREATE TABLE IF NOT EXISTS Artists (
 	artist_id SERIAL PRIMARY KEY,
+	albums VARCHAR(40) NOT NULL,
 	artist_name VARCHAR(40) NOT NULL,
-	artist_album_list VARCHAR(40) NOT NULL REFERENCES Artist_album_list(artist_album_list_id),
-	artist_genre_list VARCHAR(40) NOT NULL REFERENCES Artist_genre_list(artist_genre_ list_id)
+	genres VARCHAR(40) NOT NULL REFERENCES Genres(genre_id)
 );
 
-CREATE TABLE IF NOT EXISTS Artist_album_list (
-	artist_album_list_id SERIAL PRIMARY KEY,
-	album VARCHAR(40) NOT NULL REFERENCES Album(album_id)
-);
-
-CREATE TABLE IF NOT EXISTS Artist_genre_list (
-	artist_genre_list_id SERIAL PRIMARY KEY,
-	genre VARCHAR(40) NOT NULL REFERENCES Genre(genre_id)
+CREATE TABLE IF NOT EXISTS Artist_album (
+	artist_album_id SERIAL PRIMARY KEY,
+	artist VARCHAR(40) NOT NULL REFERENCES Artists(artist_id),
+	albums VARCHAR(40) NOT NULL REFERENCES Albums(album_id)
 );
 
 CREATE TABLE IF NOT EXISTS Album (
@@ -35,28 +28,24 @@ CREATE TABLE IF NOT EXISTS Album (
 	album_name VARCHAR(40) NOT NULL,
 	year_of_issue YEAR NOT NULL,
 	artist VARCHAR(40) NOT NULL REFERENCES Artist(artist_id)
-	track_list VARCHAR(40) NOT NULL REFERENCES Track_list(track_list_id)
-);
-
-CREATE TABLE IF NOT EXISTS Track_list (
-	track_list_id SERIAL PRIMARY KEY,
 	track VARCHAR(40) NOT NULL REFERENCES Track(track_id)
-);
 
 CREATE TABLE IF NOT EXISTS Track (
 	track_id SERIAL PRIMARY KEY,
 	track_name VARCHAR(40) NOT NULL,
-	duration SECOND 
-	album VARCHAR(40) NOT NULL REFERENCES Album(album_id)
+	album_name VARCHAR(40) NOT NULL,
+	duration SECOND,
+	collection VARCHAR(40) NOT NULL,
 );
 
-CREATE TABLE IF NOT EXISTS Track_collection (
-	collection_track_list_id SERIAL PRIMARY KEY,
-	year_of_issue VARCHAR(40) NOT NULL,	
-	track_list VARCHAR(40) NOT NULL REFERENCES Collection_track_list(collection_track_list_id)
+CREATE TABLE IF NOT EXISTS Track_list (
+	track_list_id SERIAL PRIMARY KEY,
+	track VARCHAR(40) NOT NULL REFERENCES Track(track_id),
+	collection VARCHAR(40) NOT NULL REFERENCES Collection_track_list(collection_track_list_id)
 );
 
 CREATE TABLE IF NOT EXISTS Collection_track_list (
 	collection_track_list_id SERIAL PRIMARY KEY,
-	track VARCHAR(40) NOT NULL REFERENCES Track(track_id)
+	year_of_issue VARCHAR(40) NOT NULL,	
+	track VARCHAR(40) NOT NULL REFERENCES Track(track_id) 
 );
